a05e0705483c21e2cdbc0d89351227cc3c62fce1
opposition pantophobia <162717999+Drop56796@users.noreply.github.com>
2024-05-03 22:31:44 +0800

    Create Mangle Achievement.lua

---
+++ Mangle Achievement.lua
@@ -0,0 +1,10 @@
+---====== Load achievement giver ======---
+local achievementGiver = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Custom%20Achievements/Source.lua"))()
+
+---====== Display achievement ======---
+achievementGiver({
+    Title = "Darkness Forbidden Mangle",
+    Desc = "It's Forbidden mode Surge?",
+    Reason = "Survive the Mangle",
+    Image = "rbxassetid://17369743156"
+})
